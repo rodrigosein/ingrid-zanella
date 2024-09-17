16e4827b2dea0396a738568360911c76c270da3b
Rodrigosein <rodrigosein@gmail.com>
2024-09-17 15:49:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+function openCard1() {
+  document.getElementById('cardOverlay1').classList.add('active');
+}
+
+function closeCard1() {
+  document.getElementById('cardOverlay1').classList.remove('active');
+}
+
+function openCard2() {
+  document.getElementById('cardOverlay2').classList.add('active');
+}
+
+function closeCard2() {
+  document.getElementById('cardOverlay2').classList.remove('active');
+}
+
+function openCard3() {
+  document.getElementById('cardOverlay3').classList.add('active');
+}
+
+function closeCard3() {
+  document.getElementById('cardOverlay3').classList.remove('active');
+}
+
+function openCard4() {
+  document.getElementById('cardOverlay4').classList.add('active');
+}
+
+function closeCard4() {
+  document.getElementById('cardOverlay4').classList.remove('active');
+}
+
+function openCard5() {
+  document.getElementById('cardOverlay5').classList.add('active');
+}
+
+function closeCard5() {
+  document.getElementById('cardOverlay5').classList.remove('active');
+}
+
+function openCard6() {
+  document.getElementById('cardOverlay6').classList.add('active');
+}
+
+function closeCard6() {
+  document.getElementById('cardOverlay6').classList.remove('active');
+}
+
+function openCard7() {
+  document.getElementById('cardOverlay7').classList.add('active');
+}
+
+function closeCard7() {
+  document.getElementById('cardOverlay7').classList.remove('active');
+}
+
+function openCard8() {
+  document.getElementById('cardOverlay8').classList.add('active');
+}
+
+function closeCard8() {
+  document.getElementById('cardOverlay8').classList.remove('active');
+}
